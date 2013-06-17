95c559adccfae6c58b27f1fa8b4ab60e84c63afe
unknown <Tome.Hristovski@tome_h.interworks.local>
2013-06-17 15:13:17 +0200

    Created new  Upload picture app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = 'a9280aa9838f44a7da64460baaf4c490446a28e7c6a7357b701532a8103f3c9f46add7d2e23d8e6bacd8a0705fdca3d4c98a6d31666cc7af393652cab3cb2a64'
